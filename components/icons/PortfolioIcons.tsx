6618b10ad9fe2b29179cff8518745fd90c76f13a
mfbevan <matthewbevan00@gmail.com>
2022-08-16 22:00:47 +1000

    🍱 add icons imports

--- components/icons/PortfolioIcons.tsx
+++ components/icons/PortfolioIcons.tsx
@@ -1,249 +1,497 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Tooltip } from "@chakra-ui/react";
+import { Box, Tooltip } from "@chakra-ui/react";
 import Image from "next/image";
 
 export const ReactLogo = () => (
-  <Tooltip title="React" key="React">
-    <Image src="https://Image.icons8.com/office/48/000000/react.png" />
+  <Tooltip label="React" key="React">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/office/144/000000/react.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const TypescriptLogo = () => (
-  <Tooltip title="Typescript">
-    <Image src="https://Image.icons8.com/color/48/000000/typescript.png" />
+  <Tooltip label="Typescript">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/typescript.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const TerraformLogo = () => (
-  <Tooltip title="Terraform">
-    <Image src="https://Image.icons8.com/fluency/48/000000/terraform.png" />
+  <Tooltip label="Terraform">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/fluency/144/000000/terraform.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const NodeLogo = () => (
-  <Tooltip title="Node.js">
-    <Image src="https://Image.icons8.com/color/48/000000/nodejs.png" />
+  <Tooltip label="Node.js">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/nodejs.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const GithubLogo = () => (
-  <Tooltip title="Github">
-    <Image src="https://Image.icons8.com/fluency/48/000000/github.png" />
+  <Tooltip label="Github">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/fluency/144/000000/github.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const GithubLogo64 = () => (
-  <Tooltip title="Github">
-    <Image src="https://Image.icons8.com/nolan/64/github.png" />
+  <Tooltip label="Github">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/nolan/64/github.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const GitlabLogo = () => (
-  <Tooltip title="GitLab">
-    <Image src="https://Image.icons8.com/color/48/000000/gitlab.png" />
+  <Tooltip label="GitLab">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/gitlab.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const JiraLogo = () => (
-  <Tooltip title="Jira">
-    <Image src="https://Image.icons8.com/color/48/000000/jira.png" />
+  <Tooltip label="Jira">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/jira.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const PythonLogo = () => (
-  <Tooltip title="Python">
-    <Image src="https://Image.icons8.com/color/48/000000/python--v1.png" />
+  <Tooltip label="Python">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/python--v1.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const AWSLogo = () => (
-  <Tooltip title="AWS">
-    <Image src="https://Image.icons8.com/color/48/000000/amazon-web-services.png" />
+  <Tooltip label="AWS">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/amazon-web-services.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const AzureLogo = () => (
-  <Tooltip title="Azure">
-    <Image src="https://Image.icons8.com/color/48/000000/azure-1.png" />
+  <Tooltip label="Azure">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/azure-1.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const PostgresLogo = () => (
-  <Tooltip title="PostgreSQL">
-    <Image src="https://Image.icons8.com/color/48/000000/postgreesql.png" />
+  <Tooltip label="PostgreSQL">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/postgreesql.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const MariaDbLogo = () => (
-  <Tooltip title="MariaDB">
-    <Image src="https://Image.icons8.com/color/48/000000/maria-db.png" />
+  <Tooltip label="MariaDB">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/maria-db.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const UnityLogo = () => (
-  <Tooltip title="Unity">
-    <Image src="https://Image.icons8.com/ios-filled/50/000000/unity.png" />
+  <Tooltip label="Unity">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/ios-filled/50/000000/unity.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const AndroidLogo = () => (
-  <Tooltip title="Android">
-    <Image src="https://Image.icons8.com/color/48/000000/android-os.png" />
+  <Tooltip label="Android">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/android-os.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const CSharpLogo = () => (
-  <Tooltip title="C#">
-    <Image src="https://Image.icons8.com/color/48/000000/c-sharp-logo.png" />
+  <Tooltip label="C#">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/c-sharp-logo.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const PlayStoreLogo = () => (
-  <Tooltip title="PlayStore">
-    <Image src="https://Image.icons8.com/color/48/000000/google-play.png" />
+  <Tooltip label="PlayStore">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/google-play.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const MUILogo = () => (
-  <Tooltip title="Material UI">
-    <Image src="https://Image.icons8.com/color/48/000000/material-ui.png" />
+  <Tooltip label="Material UI">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/material-ui.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const InvestmentLogo = () => (
-  <Tooltip title="Day Trading">
-    <Image
-      src="https://Image.icons8.com/external-becris-flat-becris/64/000000/external-portfolio-business-management-becris-flat-becris.png"
-      width="48px"
-      height="48px"
-    />
+  <Tooltip label="Day Trading">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/external-becris-flat-becris/64/000000/external-portfolio-business-management-becris-flat-becris.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const SolanaLogo = () => (
-  <Tooltip title="Solana">
-    <Image
-      src="https://cryptologos.cc/logos/solana-sol-logo.png"
-      width="40px"
-      height="40px"
-    />
+  <Tooltip label="Solana">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://cryptologos.cc/logos/solana-sol-logo.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const BitcoinLogo = () => (
-  <Tooltip title="Bitcoin">
-    <Image
-      src="https://Image.icons8.com/stickers/100/000000/bitcoin.png"
-      width="48px"
-      height="48px"
-    />
+  <Tooltip label="Bitcoin">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/stickers/100/000000/bitcoin.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const EthereumLogo = () => (
-  <Tooltip title="Ethereum">
-    <Image
-      src="https://Image.icons8.com/fluency/48/000000/ethereum.png"
-      width="48px"
-      height="48px"
-    />
+  <Tooltip label="Ethereum">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/fluency/144/000000/ethereum.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const LinkedInLogo = () => (
-  <Tooltip title="LinkedIn">
-    <Image src="https://Image.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png" />
+  <Tooltip label="LinkedIn">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/external-justicon-flat-justicon/64/000000/external-linkedin-social-media-justicon-flat-justicon.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const DockerLogo = () => (
-  <Tooltip title="Docker">
-    <Image src="https://Image.icons8.com/color/48/000000/docker.png" />
+  <Tooltip label="Docker">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/docker.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const CloudflareLogo = () => (
-  <Tooltip title="Cloudflare">
-    <Image src="https://Image.icons8.com/color/48/000000/cloudflare.png" />
+  <Tooltip label="Cloudflare">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/cloudflare.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const WindowsLogo = () => (
-  <Tooltip title="Windows">
-    <Image src="https://Image.icons8.com/fluency/48/000000/windows-10.png" />
+  <Tooltip label="Windows">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/fluency/144/000000/windows-10.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const MacOSLogo = () => (
-  <Tooltip title="Mac OSX">
-    <Image src="https://Image.icons8.com/color/48/000000/mac-os-logo.png" />
+  <Tooltip label="Mac OSX">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/mac-os-logo.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const LinuxLogo = () => (
-  <Tooltip title="Linux">
-    <Image src="https://Image.icons8.com/color/48/000000/linux--v1.png" />
+  <Tooltip label="Linux">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/linux--v1.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const HTMLLogo = () => (
-  <Tooltip title="HTML">
-    <Image src="https://Image.icons8.com/color/48/000000/html-5--v1.png" />
+  <Tooltip label="HTML">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/html-5--v1.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const CSSLogo = () => (
-  <Tooltip title="CSS">
-    <Image src="https://Image.icons8.com/color/48/000000/css3.png" />
+  <Tooltip label="CSS">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/css3.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const JavascriptLogo = () => (
-  <Tooltip title="Javascript">
-    <Image src="https://Image.icons8.com/color/48/000000/javascript--v2.png" />
+  <Tooltip label="Javascript">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/144/000000/javascript--v2.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const ConfluenceLogo = () => (
-  <Tooltip title="Confluence">
-    <Image src="https://Image.icons8.com/fluency/48/000000/confluence.png" />
+  <Tooltip label="Confluence">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/fluency/144/000000/confluence.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const JavaLogo = () => (
-  <Tooltip title="Java">
-    <Image src="https://Image.icons8.com/material-outlined/48/000000/java-coffee-cup-logo.png" />
+  <Tooltip label="Java">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/material-outlined/144/000000/java-coffee-cup-logo.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const SolidityLogo = () => (
-  <Tooltip title="Solidity">
-    <Image
-      src="https://Image.icons8.com/ios-filled/50/000000/solidity.png"
-      width="48px"
-      height="48px"
-    />
+  <Tooltip label="Solidity">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/ios-filled/50/000000/solidity.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const MatlabLogo = () => (
-  <Tooltip title="Matlab">
-    <Image src="https://Image.icons8.com/fluency/48/000000/matlab.png" />
+  <Tooltip label="Matlab">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/fluency/144/000000/matlab.png"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const ElrondLogo = () => (
-  <Tooltip title="Elrond">
-    <Image
-      src="https://assets.coingecko.com/coins/images/12335/large/elrond3_360.png?1626341589"
-      width="32px"
-      height="32px"
-    />
+  <Tooltip label="Elrond">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://assets.coingecko.com/coins/images/12335/large/elrond3_360.png?1626341589"
+      />
+    </Box>
   </Tooltip>
 );
 
 export const TwitterLogo = () => (
-  <Tooltip title="mfbevan">
-    <Image src="https://Image.icons8.com/color/64/twitter--v1.png" />
+  <Tooltip label="mfbevan">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/64/twitter--v1.png"
+      />
+    </Box>
+  </Tooltip>
+);
+
+export const KubernetesLogo = () => (
+  <Tooltip label="Kubernetes">
+    <Box>
+      <Image
+        height={48}
+        width={48}
+        layout="fixed"
+        src="https://img.icons8.com/color/288/kubernetes.png"
+      />
+    </Box>
   </Tooltip>
 );
