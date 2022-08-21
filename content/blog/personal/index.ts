1bf1c403cbddad26becb89526c6824a7fb57a7e7
mfbevan <matthewbevan00@gmail.com>
2022-08-22 00:05:40 +1000

    📝 post: mfbevan.eth

--- content/blog/personal/index.ts
+++ content/blog/personal/index.ts
@@ -1,7 +1,7 @@
-import { GrRobot } from "react-icons/gr"
+import { FaEthereum } from "react-icons/fa"
 import { IconType } from "react-icons"
 
-import Post20220820 from "./20220820.mdx"
+import Post20220821 from "./20220821.mdx"
 
 export interface IPost {
   name: string
@@ -12,25 +12,11 @@ export interface IPost {
 }
 
 export const personalPosts: Record<string, IPost> = {
-  "20220820": {
-    name: "My first post",
-    date: new Date("20 Aug 2022"),
-    summary: "This is the summary of my first post",
-    post: Post20220820,
-    Icon: GrRobot,
-  },
   "20220821": {
-    name: "My second post",
+    name: "mfbevan.eth",
     date: new Date("21 Aug 2022"),
-    summary: "This is the summary of my second post",
-    post: Post20220820,
-    Icon: GrRobot,
-  },
-  "20220822": {
-    name: "My third post",
-    date: new Date("22 Aug 2022"),
-    summary: "This is the summary of my third post",
-    post: Post20220820,
-    Icon: GrRobot,
+    summary: "Bachelors to Blockchain",
+    post: Post20220821,
+    Icon: FaEthereum,
   },
 }
