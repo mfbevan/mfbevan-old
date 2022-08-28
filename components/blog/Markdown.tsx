83e57645b3d384ec09b5128ffca544f7660e4e43
mfbevan <matthewbevan00@gmail.com>
2022-08-28 23:00:36 +1000

    📝 blog: create conways's game of life post

--- components/blog/Markdown.tsx
+++ components/blog/Markdown.tsx
@@ -1,19 +1,43 @@
-import { Heading, Code, Box, Text, Link } from "@chakra-ui/react"
+import { Heading, Code, Box, Text, Link, Image } from "@chakra-ui/react";
 
-type PropType = JSX.IntrinsicAttributes
+type PropType = JSX.IntrinsicAttributes;
 
 const sharedProps = {
   py: 2,
-}
+};
+
+const Blockquote = (props: PropType) => (
+  <Box pl={3} pr={2} ml={2} borderLeft="4px" borderColor="gray.300" bgColor="gray.50">
+    <Text {...props} />
+  </Box>
+);
 
 export const Markdown = {
-  h1: (props: PropType) => <Heading fontSize="4xl" {...sharedProps} {...props} />,
-  h2: (props: PropType) => <Heading fontSize="3xl" {...sharedProps} {...props} />,
-  h3: (props: PropType) => <Heading fontSize="2xl" {...sharedProps} {...props} />,
-  h4: (props: PropType) => <Heading fontSize="xl" {...sharedProps} {...props} />,
+  h1: (props: PropType) => (
+    <Heading fontSize="4xl" {...sharedProps} {...props} />
+  ),
+  h2: (props: PropType) => (
+    <Heading fontSize="3xl" {...sharedProps} {...props} />
+  ),
+  h3: (props: PropType) => (
+    <Heading fontSize="2xl" {...sharedProps} {...props} />
+  ),
+  h4: (props: PropType) => (
+    <Heading fontSize="xl" {...sharedProps} {...props} />
+  ),
   p: (props: PropType) => <Text {...sharedProps} {...props} />,
-  li: (props: PropType) => <Box pl={8}><li {...props} /></Box>,
-  ul: (props: PropType) => <Box pl={8}><ul {...props} /></Box>,
+  li: (props: PropType) => (
+    <Box pl={8}>
+      <li {...props} />
+    </Box>
+  ),
+  ul: (props: PropType) => (
+    <Box>
+      <ul {...props} />
+    </Box>
+  ),
   code: (props: PropType) => <Code {...props} />,
-  a: (props: PropType) => <Link isExternal textColor="blue.400" {...props} />
-}
+  a: (props: PropType) => <Link isExternal textColor="blue.400" {...props} />,
+  blockquote: (props: PropType) => <Blockquote {...props} />,
+  img: (props: PropType) => <Image {...props} alt={"mdx-image"} mx="auto"/>
+};
--- content/blog/personal/index.ts
+++ content/blog/personal/index.ts
@@ -1,14 +1,18 @@
-import { FaEthereum } from "react-icons/fa"
-import { IconType } from "react-icons"
+import { FaEthereum } from "react-icons/fa";
+import { GiConwayLifeGlider } from "react-icons/gi";
+import { IconType } from "react-icons";
 
-import Post20220821 from "./20220821.mdx"
+import Post20220821 from "./20220821.mdx";
+import Post20220827 from "./20220827.mdx";
+
+console.log(Post20220827);
 
 export interface IPost {
-  name: string
-  date: Date
-  summary: string
-  post(props: any): JSX.Element
-  Icon: IconType
+  name: string;
+  date: Date;
+  summary: string;
+  post(props: any): JSX.Element;
+  Icon: IconType;
 }
 
 export const personalPosts: Record<string, IPost> = {
@@ -19,4 +23,12 @@ export const personalPosts: Record<string, IPost> = {
     post: Post20220821,
     Icon: FaEthereum,
   },
-}
+  "20220827": {
+    name: "Conway's Game of Life",
+    date: new Date("28 Aug 2022"),
+    summary:
+      "An Ethereum smart contract implementation of John Conway's cellular automaton.",
+    post: Post20220827,
+    Icon: GiConwayLifeGlider,
+  },
+};
