5ebd83a57dc2a9356ab28c9cb4c7d3a436ca7ea9
mfbevan <matthewbevan00@gmail.com>
2022-09-06 21:26:27 +1000

    🔥 remove: unfinished post 20220827

--- content/blog/personal/index.ts
+++ content/blog/personal/index.ts
@@ -1,11 +1,7 @@
 import { FaEthereum } from "react-icons/fa";
-import { GiConwayLifeGlider } from "react-icons/gi";
 import { IconType } from "react-icons";
 
 import Post20220821 from "./20220821.mdx";
-import Post20220827 from "./20220827.mdx";
-
-console.log(Post20220827);
 
 export interface IPost {
   name: string;
@@ -22,13 +18,5 @@ export const personalPosts: Record<string, IPost> = {
     summary: "A bit about me, and how I got here.",
     post: Post20220821,
     Icon: FaEthereum,
-  },
-  "20220827": {
-    name: "Conway's Game of Life",
-    date: new Date("28 Aug 2022"),
-    summary:
-      "An Ethereum smart contract implementation of John Conway's cellular automaton.",
-    post: Post20220827,
-    Icon: GiConwayLifeGlider,
   },
 };
