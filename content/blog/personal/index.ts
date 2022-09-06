4e79b423a299a55051c48a91f708876c207cff8d
mfbevan <matthewbevan00@gmail.com>
2022-09-06 21:24:42 +1000

    📝  post: 20220906 init

--- content/blog/personal/index.ts
+++ content/blog/personal/index.ts
@@ -1,14 +1,16 @@
-import { FaEthereum } from "react-icons/fa"
-import { IconType } from "react-icons"
+import { FaEthereum } from "react-icons/fa";
+import { GiCyberEye } from "react-icons/gi";
+import { IconType } from "react-icons";
 
-import Post20220821 from "./20220821.mdx"
+import Post20220821 from "./20220821.mdx";
+import Post20220906 from "./20220906.mdx";
 
 export interface IPost {
-  name: string
-  date: Date
-  summary: string
-  post(props: any): JSX.Element
-  Icon: IconType
+  name: string;
+  date: Date;
+  summary: string;
+  post(props: any): JSX.Element;
+  Icon: IconType;
 }
 
 export const personalPosts: Record<string, IPost> = {
@@ -19,4 +21,11 @@ export const personalPosts: Record<string, IPost> = {
     post: Post20220821,
     Icon: FaEthereum,
   },
-}
+  "20220906": {
+    name: "Look but don't touch",
+    date: new Date("06 Sep 2022"),
+    summary: "The under-used power of view functions in Solidity.",
+    post: Post20220906,
+    Icon: GiCyberEye,
+  },
+};
