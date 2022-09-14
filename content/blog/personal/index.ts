d7d71970b994ecf1981429285f0ef6560edf41f1
mfbevan <matthewbevan00@gmail.com>
2022-09-14 20:29:21 +1000

    🔥 post: remove unfinished post

--- content/blog/personal/index.ts
+++ content/blog/personal/index.ts
@@ -1,9 +1,7 @@
 import { FaEthereum } from "react-icons/fa";
-import { GiCyberEye } from "react-icons/gi";
 import { IconType } from "react-icons";
 
 import Post20220821 from "./20220821.mdx";
-import Post20220906 from "./20220906.mdx";
 
 export interface IPost {
   name: string;
@@ -20,12 +18,5 @@ export const personalPosts: Record<string, IPost> = {
     summary: "A bit about me, and how I got here.",
     post: Post20220821,
     Icon: FaEthereum,
-  },
-  "20220906": {
-    name: "Look but don't touch",
-    date: new Date("06 Sep 2022"),
-    summary: "The under-used power of view functions in Solidity.",
-    post: Post20220906,
-    Icon: GiCyberEye,
   },
 };
