bec79415106aa3ebd803053e5f1a6a317ab58a9a
mfbevan <matthewbevan00@gmail.com>
2022-09-29 22:20:23 +1000

    💄 refactor: restyle post summary page

--- components/blog/BlogPostSummary.tsx
+++ components/blog/BlogPostSummary.tsx
@@ -6,11 +6,13 @@ import {
   useColorModeValue,
   Text,
   HStack,
-  Spacer,
+  Icon as ChakraIcon,
   useBreakpoint,
+  Flex,
 } from "@chakra-ui/react";
 import { IPost } from "../../content/blog/personal";
 import { BsChevronDoubleRight } from "react-icons/bs";
+import { ChevronRightIcon } from "@chakra-ui/icons";
 
 const BlogPostSummary = ({
   postKey,
@@ -23,37 +25,65 @@ const BlogPostSummary = ({
 
   return (
     <Center py={2}>
-      <Link href={`/blog/${postKey}`}>
-        <Box
-          w={useBreakpoint()}
-          boxShadow="md"
-          rounded="md"
-          textAlign="left"
-          mx={2}
-          p={4}
-          bg={useColorModeValue("white", "gray.700")}
+      <Box
+        w={useBreakpoint()}
+        boxShadow="md"
+        rounded="md"
+        textAlign="left"
+        mx={2}
+        p={4}
+        bg={useColorModeValue("white", "gray.700")}
+      >
+        <Link
+          href={`/blog/${postKey}`}
+          role={"group"}
+          display={"block"}
+          p={2}
+          rounded={"md"}
+          _hover={{ bg: useColorModeValue("blue.50", "gray.900") }}
         >
-          <HStack spacing={4}>
-            <Icon
-              size={30}
-              color="blue.800"
-              opacity="0.2"
-            />
+          <HStack align={"center"}>
+          <ChakraIcon
+                color={"blue.500"}
+                _groupHover={{ opacity: "0.6" }}
+                opacity="0.2"
+                w={7}
+                h={7}
+                as={Icon}
+              />
             <Box>
-              <Heading fontSize="2xl" fontWeight={500}>
+              <Heading
+                fontSize="xl"
+                transition={"all .3s ease"}
+                _groupHover={{ color: "blue.500" }}
+                fontWeight={500}
+              >
                 {name}
               </Heading>
               <Heading fontSize="xs" fontWeight={500}>
                 {date.toDateString()}
               </Heading>
-              <Text>{summary}</Text>
+              <Text fontSize={"sm"}>{summary}</Text>
             </Box>
-            <Spacer />
-
-            <BsChevronDoubleRight />
+            <Flex
+              transition={"all .3s ease"}
+              transform={"translateX(-10px)"}
+              opacity={0}
+              _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+              justify={"flex-end"}
+              align={"center"}
+              flex={1}
+            >
+              <ChakraIcon
+                color={"blue.500"}
+                w={5}
+                h={5}
+                as={ChevronRightIcon}
+              />
+            </Flex>
           </HStack>
-        </Box>
-      </Link>
+        </Link>
+      </Box>
     </Center>
   );
 };
