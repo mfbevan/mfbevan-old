789cc443808f19489a3a840a14fc09d6dac51a0c
mfbevan <matthewbevan00@gmail.com>
2022-11-05 20:48:54 +1000

    refactor: 🔥 remove rainbow from header

--- components/navigation/Socials.tsx
+++ components/navigation/Socials.tsx
@@ -1,9 +1,8 @@
 import { HStack } from "@chakra-ui/react";
-import { GithubLinkLogo, LinkedInLinkLogo, RainbowLinkLogo, TwitterLinkLogo } from "../icons";
+import { GithubLinkLogo, LinkedInLinkLogo, TwitterLinkLogo } from "../icons";
 
 const Socials = () => (
   <HStack spacing={2} mr={4} h="48px">
-    <RainbowLinkLogo />
     <GithubLinkLogo />
     <LinkedInLinkLogo />
     <TwitterLinkLogo />
