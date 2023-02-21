72f5bf8e474f53b984ab9112ea685eb410787ee5
mfbevan <matthewbevan00@gmail.com>
2023-02-21 18:41:22 +1000

    feat: 🐍 contribution snake

--- components/blog/Markdown.tsx
+++ components/blog/Markdown.tsx
@@ -1,4 +1,12 @@
-import { Heading, Code, Box, Text, Link, Image, useColorModeValue } from "@chakra-ui/react";
+import {
+  Heading,
+  Code,
+  Box,
+  Text,
+  Link,
+  Image,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 type PropType = JSX.IntrinsicAttributes;
 
@@ -7,14 +15,22 @@ const sharedProps = {
 };
 
 const Blockquote = (props: PropType) => (
-  <Box pl={3} pr={2} ml={2} my={2} borderLeft="4px" borderColor="gray.300" bgColor={useColorModeValue("gray.50", "gray.700")}>
+  <Box
+    pl={3}
+    pr={2}
+    ml={2}
+    my={2}
+    borderLeft="4px"
+    borderColor="gray.300"
+    bgColor={useColorModeValue("gray.50", "gray.700")}
+  >
     <Text {...props} />
   </Box>
 );
 
 export const Markdown = {
   h1: (props: PropType) => (
-    <Heading fontSize="3xl" fontWeight={1000}{...sharedProps} {...props} />
+    <Heading fontSize="3xl" fontWeight={1000} {...sharedProps} {...props} />
   ),
   h2: (props: PropType) => (
     <Heading fontSize="2xl" fontWeight={800} {...sharedProps} {...props} />
@@ -39,5 +55,5 @@ export const Markdown = {
   code: (props: PropType) => <Code {...props} />,
   a: (props: PropType) => <Link isExternal textColor="blue.400" {...props} />,
   blockquote: (props: PropType) => <Blockquote {...props} />,
-  img: (props: PropType) => <Image {...props} alt={"mdx-image"} mx="auto"/>
+  img: (props: PropType) => <Image {...props} alt={"mdx-image"} mx="auto" />,
 };
